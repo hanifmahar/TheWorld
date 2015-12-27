5933c80529549f6ec4236deec8ec470c3ece16b5
hanifmahar <hanif.mahar@gmail.com>
2015-12-27 18:49:48 -0500

    Using Validations

--- src/TheWorld/ViewModels/ContactViewModel.cs
+++ src/TheWorld/ViewModels/ContactViewModel.cs
@@ -1,10 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TheWorld.ViewModels
 {
     public class ContactViewModel
     {
+        [Required] //Data Annotations 
+        [StringLength (255, MinimumLength =5)]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
         public string Message { get; set; }
     }
 }
