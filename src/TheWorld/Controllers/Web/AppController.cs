92a618ea4d9f9b02ede9f1595d18dd6cf63d29fb
hanifmahar <hanif.mahar@gmail.com>
2015-12-27 19:35:31 -0500

    Adding a service

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNet.Mvc;
 using System;
+using TheWorld.Services;
 using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController:Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService sevice)
+        {
+            _mailService = sevice;
+        }
         public IActionResult Index()
         {
             return View();
@@ -23,6 +30,8 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact( ContactViewModel model )
         {
+            _mailService.SendMail("","",
+                $"Contact Page from {model.Email }",model.Message );
             return View();
         }
     }
---
+++ src/TheWorld/Services/DebugMailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TheWorld.Services
+{
+    public class DebugMailService : IMailService
+    {
+        public bool SendMail(string to, string from, string subject, string body)
+        {
+            Debug.WriteLine($"Sending mail: To:{to}, from : {from}, Subject: {subject} ");
+
+            return true;
+        }
+    }
+}
