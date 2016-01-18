6db8a3d57515e0489f67628a8027bb9f50649aca
hanifmahar <hanif.mahar@gmail.com>
2016-01-18 18:11:53 -0500

    Camel Case Property Names Contract Resolver

--- src/TheWorld/Controllers/Api/TripController.cs
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -3,15 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Models;
 
 namespace TheWorld.Controllers.Api
 {
     public class TripController : Controller
     {
+        private IWorldRepository _repository;
+
+        public TripController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+
         [HttpGet("api/trips")]
         public JsonResult Get()
         {
-            return Json( new { name="Muhammad Habib Mahar" } );
+            var results = _repository.GetAllTripsWithStops();
+
+            return Json(results);
         }
     }
 }
